54cb724819cc8a7da4b6f25af6299cdafa654243
Alida <alida@uidaho.edu>
2014-09-08 14:01:30 -0700

    added sample inputs

--- server.R
+++ server.R
@@ -1,3 +1,5 @@
+#this script is running on bioinfo-mite currently, most updated version
+
 library(shiny)
 
 shinyServer(function(input, output) {
@@ -16,10 +18,11 @@ shinyServer(function(input, output) {
     paste("You have selected", input$Reads, "sequencing.")})
 
   output$Length <- renderText({
-    paste("You have selected", input$Length, "reads.")})
+    paste("You have selected", input$Length, "bp reads.")})
   
-  output$Indices <- renderText({
-    paste(input$Indices, "barcodes were selected as your indices.")})
+  output$Samples <- renderText({
+    paste(input$Samples, "samples will be sequenced.")})
+    
 
   sequences <- function(x){
     x <- input$Reads
@@ -28,25 +31,29 @@ shinyServer(function(input, output) {
     else if (x != "Single-end")
       print(c(input$Length, input$Length), sep="")
   }
-  
-  date.input <- Sys.Date()
-  today.date <- format(date.input, format="%D")
+
+date.input <- Sys.Date()
+today.date <- format(date.input, format="%D")
   
 #a reactive function that takes the user-supplied input and writes a .csv when downloads are requested
 datasetInput <- reactive({
   c("[Header]", "IEMFileVersion,4", paste("Investigator Name", input$Name, sep=","), 
-    paste("Experiment Name", input$ProjID, sep=","), paste("Date", today.date, sep=","), "Workflow", 
+    paste("Experiment Name", input$ProjID, sep=","), paste("Date", today.date, sep=","), "Workflow,GenerateFASTQ", 
     "Application", paste("Assay", input$Assay, sep=","), "Description", "Chemistry,Default", "", 
     "[Reads]", sequences(x), "",
-    "[Settings]", "ReverseComplement,0", "", "[Data]")
-})
+    "[Settings]", "ReverseComplement,0", "", "[Data]", c("Sample_ID,Sample_Name,Sample_Plate,Sample_Well,I7_Index_ID,index,Sample_Project,Description"),
+  c(paste(input$Sample_ID1, input$Sample_ID1, ",,", input$Index1, input$ProjID, input$ProjID, sep=","))
+  )})
 
 
 # downloads a .csv file in the correct format to pass to the Miseq using variables determined above
-output$toMiSeq <- downloadHandler(
+output$downloadData <- downloadHandler(
   filename = function() {
-    paste(input$Name, '.csv', sep='') },
-    content = function(con) {writeLines(datasetInput(), "Alida.csv");
-      write.csv(datasetInput(), con)}
+    paste(input$Name, today.date, '.csv', sep='')
+    },
+    content = function(file) {
+      write.table(datasetInput(), file, quote=F, row.names=F, col.names=F)
+    }
 )
+
 })
--- ui.R
+++ ui.R
@@ -1,3 +1,8 @@
+#this script is running on bioinfo-mite currently, most updated version
+
+bcds <- read.table("/home/alida/Documents/Idaho/R/sample_sheet/toposheet/grc.barcodes.csv", header=T,
+                   stringsAsFactors=F)
+bcd.name <- (bcds$grc.name)
 
 library(shiny)
 
@@ -13,10 +18,10 @@ shinyUI(fluidPage(
                 textOutput("Assay"),
                 textOutput("Reads"),
                 textOutput("Length"),
-                textOutput("Indices"),
+                textOutput("Samples"),
                 br()),
                 
-  mainPanel(h2("Genomics Resources Core MiSeq Sequencing Submissions", align="center"),
+  mainPanel(h2("Genomics Resources Core Sequencing Manager", align="center"),
             
   textInput("Name", label = h3("Project Leader Name"), value = ""),
   textInput("ProjID", label = h3("Project Name"), value = ""),
@@ -29,13 +34,36 @@ shinyUI(fluidPage(
   selectInput("Length", label = h3("Select Length of Reads"), 
               choices = list("50", "75", "100", "150", "200", "300"), 
               selected = 1),
-  selectInput("Indices", label = h3("Select indexing barcodes"), 
-              choices = list("Alpha", "Bravo", "Charlie", "Delta", "Echo",
-                             "Foxtrot", "Golf", "Hotel", "India", "Juliet",
-                             "Lima"), selected = 1),
-  fileInput("Barcodes", label = h3("Barcode File Input")),
-  actionButton("Submit", label = "Submit to GRC"),
-  downloadButton("toMiSeq", label = "Download a MiSeq submission sheet"),
+  br(),
+
+  numericInput("Samples", label = h5("Input the number of samples for
+                                     sequencing:"), value = NA, step=NA),
+  br(),
+  br(),
+  br(),
+  wellPanel(textInput("Sample_ID1", label = h5("Enter sample 1 name")),
+            selectInput("Index1", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  wellPanel(textInput("Sample_ID2", label = h5("Enter sample 2 name")),
+            selectInput("Index2", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  wellPanel(textInput("Sample_ID3", label = h5("Enter sample 3 name")),
+            selectInput("Index3", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  wellPanel(textInput("Sample_ID4", label = h5("Enter sample 4 name")),
+            selectInput("Index4", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  wellPanel(textInput("Sample_ID5", label = h5("Enter sample 5 name")),
+            selectInput("Index5", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  wellPanel(textInput("Sample_ID6", label = h5("Enter sample 6 name")),
+            selectInput("Index6", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  wellPanel(textInput("Sample_ID7", label = h5("Enter sample 7 name")),
+            selectInput("Index7", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  wellPanel(textInput("Sample_ID8", label = h5("Enter sample 8 name")),
+            selectInput("Index8", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  wellPanel(textInput("Sample_ID9", label = h5("Enter sample 9 name")),
+            selectInput("Index9", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  wellPanel(textInput("Sample_ID10", label = h5("Enter sample 10 name")),
+            selectInput("Index10", label = h5("Select I7 indexing barcode"), choices = bcd.name)),
+  br(),
+  br(),
+  downloadButton("downloadData", "Download the submission sheet"),
   hr()
                 )
 
