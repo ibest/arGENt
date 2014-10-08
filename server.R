e46c6fe5ec2ff7291a7a5797e8fc88c42d4b445a
Alida <alida@uidaho.edu>
2014-10-08 09:58:48 -0700

    branched to take a completely new direction in uder uploads

--- server.R
+++ server.R
@@ -1,63 +1,31 @@
-#this script is running on bioinfo-mite currently: unsure of whether or not to integrate a dynamic user input for number of samples due to issues with keeping and exporting that information, so keeping it static for now
-
 library(shiny)
-bcds.p7 <- read.table("barcode_p7_lookup.txt", col.names=F, stringsAsFactors=F, sep=",")
-bcds.p5 <- read.table("barcode_p5_lookup.txt", col.names=F, stringsAsFactors=F, sep=",")
-bcd_p7 <- (bcds.p7)
-bcd_p5 <- (bcds.p5)
+
+sg1 <- read.csv("shotgun_examplesheet.csv", header=T)
+amp1 <- read.csv("dbcamplicon_examplesheet.csv", header=T)
 
 shinyServer(function(input, output) {
   
-  # the reactive ext in the sidebar that changes when a user makes a selection
-  output$Name <- renderText({ 
-    paste(input$Name, "is the project leader.")})
+  output$out1 <- renderPrint({
+    inFile <- input$file
+    inFile$name})
   
-  output$ProjID <- renderText({
-    paste(input$ProjID, "is the project name.")})
+  output$out2 <- renderPrint({
+    inFile <- input$file
+    inFile$datapath})
   
-  output$Assay <- renderText({
-    paste(input$Assay, "was used to prepare the libraries.")})
-  
-  output$Reads <- renderText ({ 
-    paste("You have selected", input$Reads, "sequencing.")})
-
-  output$Length <- renderText({
-    paste("You have selected", input$Length, "bp reads.")})
-  
-  output$Samples <- renderText({
-    paste(input$Samples, "samples will be sequenced.")})
+  output$contents <- renderTable({
     
-
-  sequences <- function(x){
-    x <- input$Reads
-    if (x == "Single-end")
-    print(c(input$Length, "0"), sep="")
-    else if (x != "Single-end")
-      print(c(input$Length, input$Length), sep="")
-  }
+    inFile <- input$file
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    data <- do.call(rbind, lapply(inFile$datapath, function(M){
+      ws <-read.csv(M, header=TRUE)
+    }))
+    head(data, n = input$obs)
+  })
   
-date.input <- Sys.Date()
-today.date <- format(date.input, format="%D")
-  
-#a reactive function that takes the user-supplied input and writes a .csv when downloads are requested
-datasetInput <- reactive({
-  c("[Header]", "IEMFileVersion,4", paste("Investigator Name", input$Name, sep=","), 
-    paste("Experiment Name", input$ProjID, sep=","), paste("Date", today.date, sep=","), "Workflow,GenerateFASTQ", 
-    "Application,FASTQ Only", paste("Assay", input$Assay, sep=","), paste("Description,", today.date, input$Name, input$ProjID, sep=""), 
-    "Chemistry,Default", "", "[Reads]", sequences(x), "",
-    "[Settings]", "ReverseComplement,0", "", "[Data]", c("Sample_ID,Sample_Name,Sample_Plate,Sample_Well,I7_Index_ID,index,Sample_Project,Description"),
-  c(paste(input$Sample_ID1, input$Sample_ID1, ",", input$Index1, input$ProjID, input$ProjID, sep=","))
-  )})
-
-
-# downloads a .csv file in the correct format to pass to the Miseq using variables determined above
-output$downloadData <- downloadHandler(
-  filename = function() {
-    paste(input$Name, today.date, '.csv', sep='')
-    },
-    content = function(file) {
-      write.table(datasetInput(), file, quote=F, row.names=F, col.names=F)
-    }
-)
-
+  output$sg <- renderTable({sg1})
+  output$amp <- renderTable({amp1})
 })
--- ui.R
+++ ui.R
@@ -1,86 +1,29 @@
-#this script is running on bioinfo-mite currently: unsure of whether or not to integrate a dynamic user input for number of samples due to issues with keeping and exporting that information, so keeping it static for now
-
 library(shiny)
-
-bcds.p7 <- read.table("barcode_p7_lookup.txt", col.names=F, stringsAsFactors=F, sep=",")
-bcds.p5 <- read.table("barcode_p5_lookup.txt", col.names=F, stringsAsFactors=F, sep=",")
-bcd_p7 <- (bcds.p7)
-bcd_p5 <- (bcds.p5)
-
 
 shinyUI(fluidPage(
   
-  titlePanel(img(src="ibest_logo.jpg", align="center")),
+  #Application title
+  titlePanel("Sample Sheet Viewer"),
   
-  sidebarLayout(position="right",
-                sidebarPanel(
-                  br(),
-                  textOutput("Name"),
-                  textOutput("ProjID"),
-                  textOutput("Assay"),
-                  textOutput("Reads"),
-                  textOutput("Length"),
-                  textOutput("Indices"),
-                  br()),
+  #Sidebar with controls to select a dataset and specify the number of observations to view
   
-  mainPanel(h2("Genomics Resources Core MiSeq Sequencing Submissions", align="center"),                  
-    textInput("Name", label = h3("Project Leader Name"), value = ""),
-    textInput("ProjID", label = h3("Project Name"), value = ""),
-    selectInput("Assay", label = h3("Select Assay Type"), 
-              choices = list("TruSeq HT", "TruSeq LT", "Nextera XT"), 
-              selected = 1),
-    radioButtons("Reads", label = h3("Single/Paired"),
-               choices = list("Single-end", "Paired-end"), 
-               selected = 1),
-    selectInput("Length", label = h3("Select Length of Reads"), 
-              choices = list("50", "75", "100", "150", "200", "300"), 
-              selected = 1),
-    br(),
-    checkboxInput("DBC", label = h3("Check box for double barcodes")),
-    br(),
-    selectInput("numSamples", label = h4("Select the number of samples for
-                                     sequencing and enter information below:"), choices =list("1-5" = 1, "6-10" = 2, "11-15" = 3, "16-20" =4)),
-    br(),
-    br(),
-    br(),
-    wellPanel(textInput("Sample_ID1", label = h5("Enter sample name")),
-            selectInput("Index1", label = h5("Select I7 indexing barcode"), choices = bcd_p7),
-            conditionalPanel(
-              condition = "input.DBC==true",
-              selectInput("I5Index1", label = h5("Select I5 indexing barcode"), choices = bcd_p5))),
-    wellPanel(textInput("Sample_ID2", label = h5("Enter sample name")),
-              selectInput("Index2", label = h5("Select I7 indexing barcode"), choices = bcd_p7),
-              conditionalPanel(
-                condition = "input.DBC==true",
-                selectInput("I5Index2", label = h5("Select I5 indexing barcode"), choices = bcd_p5))),
-    wellPanel(textInput("Sample_ID3", label = h5("Enter sample name")),
-              selectInput("Index3", label = h5("Select I7 indexing barcode"), choices = bcd_p7),
-              conditionalPanel(
-                condition = "input.DBC==true",
-                selectInput("I5Index3", label = h5("Select I5 indexing barcode"), choices = bcd_p5))),
-    wellPanel(textInput("Sample_ID4", label = h5("Enter sample name")),
-              selectInput("Index4", label = h5("Select I7 indexing barcode"), choices = bcd_p7),
-              conditionalPanel(
-                condition = "input.DBC==true",
-                selectInput("I5Index4", label = h5("Select I5 indexing barcode"), choices = bcd_p5))),
-    wellPanel(textInput("Sample_ID1", label = h5("Enter sample name")),
-              selectInput("Index5", label = h5("Select I7 indexing barcode"), choices = bcd_p7),
-              conditionalPanel(
-                condition = "input.DBC==true",
-                selectInput("I5Index5", label = h5("Select I5 indexing barcode"), choices = bcd_p5))),
-    conditionalPanel(
-     condition = "input.numSamples == 2",
-     wellPanel(textInput("Sample_ID6", label = h5("Enter sample 6 name")),
-               selectInput("Index6", label = h5("Select I7 indexing barcode"), choices = bcd_p7),
-               conditionalPanel(
-                 condition = "input.DBC==true",
-                 selectInput("I5Index6", label = h5("Select I5 indexing barcode"), choices = bcd_p5)))),
-    br(),
-    paste("Double-check all barcode assignments!"),
-    br(),
-    br(),
-    downloadButton("downloadData", label=h5("Download the submission sheet")),
-    hr()
-
-  ))
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", 
+                'Upload a .csv file to the core and view it here:', 
+                multiple=TRUE, 
+                accept=c(".csv")),
+      numericInput("obs", 
+                   "Number of lines to view:",
+                   10)
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Shotgun Sequencing", value = 1, tableOutput("sg"), helpText("Follow the format listed")),
+        tabPanel("Amplicon Sequencing", value = 2, tableOutput("amp"), helpText("The Barcode IDs are provided by the GRC; refer to the barcode lookup tables to determine the appropriate IDs.")), id = 1),
+      verbatimTextOutput('out1'),
+      tableOutput('contents')
+    )
+  )
 ))
